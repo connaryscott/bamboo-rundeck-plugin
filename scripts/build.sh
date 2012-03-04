cdc8db4679883db5cc9d59dc955bad99c5db8b06
Chuck Scott <connaryscott@gmail.com>
2012-03-03 16:12:45 -0800

    fixed bogus build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -44,7 +44,8 @@ RUNDECK_PLUGIN_NAME=bamboo-rundeck-plugin
 
 RUNDECK_PLUGIN_SCM_URL=git://github.com/connaryscott/${RUNDECK_PLUGIN_NAME}.git
 ATLAS_SDK=atlassian-plugin-sdk
-ATLAS_SDK_VERSION=3.7.3
+#ATLAS_SDK_VERSION=3.7.3
+ATLAS_SDK_VERSION=3.8
 ATLAS_SDK_TGZ=${ATLAS_SDK}-${ATLAS_SDK_VERSION}.tar.gz
 ATLAS_SDK_URL=https://maven.atlassian.com/content/repositories/atlassian-public/com/atlassian/amps/${ATLAS_SDK}/${ATLAS_SDK_VERSION}/${ATLAS_SDK_TGZ}
 
@@ -82,32 +83,8 @@ fi
 
 export PATH=${ATLAS_SDK_ROOT}/bin:${PATH}
 
-if [ ! -d ${RUNDECK_PLUGIN_NAME} ]
+if ! ${ATLAS_MVN} -Dmaven.test.skip=false clean install
 then
-   if ! git clone ${RUNDECK_PLUGIN_SCM_URL}
-   then
-      echo "unable to clone ${RUNDECK_PLUGIN_SCM_URL}" 1>&2
-      exit 1
-   fi
-fi
-pushd ${RUNDECK_PLUGIN_NAME}
-   if ! git checkout ${BRANCH}
-   then
-      echo "unable to update ${BRANCH} via git checkout" 1>&2
-      exit 1
-   fi
-popd
-
-if [ ! -d ${RUNDECK_PLUGIN_NAME} ]
-then
-   echo "directory ${RUNDECK_PLUGIN_NAME} does not appear to exist" 1>&2
-   exit 1
-fi
-
-pushd ${RUNDECK_PLUGIN_NAME}  || { echo "unable to pushd into $(pwd)/${RUNDECK_PLUGIN_NAME}" 1>&2; exit 1; }
-   if ! ${ATLAS_MVN} -Dmaven.test.skip=false clean install
-   then
       echo "failed executing ${ATLAS_MVN} -Dmaven.test.skip=false clean install" 1>&2
       exit 1
-   fi
-popd
+fi
