32aa71cc2e35ec97d17881f105b68f70a7d72450
chuck <chuck@sdp-centos-63-64-1.local>
2013-05-05 14:54:58 -0700

    added simple releaseit script for bintray mavenrepo

---
+++ releaseit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+
+
+curl -k -T target/bamboo-rundeck-plugin-1.1.jar  -u'connaryscott:755e349fa963fe1beb1ee136f2f5c69d9f2d8aa893f321ced175d302a575647e' https://api.bintray.com/content/connaryscott/maven-releases/bamboo-rundeck-plugin/1.1//bamboo-rundeck-plugin-1.1.jar
